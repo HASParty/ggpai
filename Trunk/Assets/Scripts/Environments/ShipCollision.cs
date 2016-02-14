bd0042b4f20c49be4479cea72aeb1416057c9aba
HafdisE <hafdiseh@gmail.com>
2016-02-14 20:51:07 +0000

    Too fancy

---
+++ Trunk/Assets/Scripts/Environments/ShipCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipCollision : MonoBehaviour {
+    private AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        audio.Play();
+    }
+}
