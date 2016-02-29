a692f439d9784997d2c9ebb8fced7af84670431a
Hafdis Erla <hafdiseh@gmail.com>
2016-02-29 20:28:20 +0000

    missing file from previous commit

---
+++ Trunk/Assets/UIMoodViewer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Boardgame.Agent;
+using UnityEngine.UI;
+using Boardgame.Configuration;
+
+namespace Boardgame.UI {
+    public class UIMoodViewer : MonoBehaviour {
+
+        private PersonalityModule pm;
+        public Text MoodText;
+        public RectTransform ArousalBG;
+        public RectTransform ArousalKnob;
+        public RectTransform ValenceBG;
+        public RectTransform ValenceKnob;
+
+
+        // Use this for initialization
+        void Start() {
+            pm = FindObjectOfType<PersonalityModule>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            MoodText.text = pm.GetEmotion().ToString();
+            Set(ArousalBG, ArousalKnob, pm.GetArousal());
+            Set(ValenceBG, ValenceKnob, pm.GetValence());
+        }
+
+        public void Set(RectTransform bg, RectTransform knob, float val) {
+            float frac = (val - Config.Low) / Config.High;
+            float x = bg.rect.width * frac;
+            knob.anchoredPosition = new Vector2(x, knob.anchoredPosition.y);
+        }
+    }
+}
