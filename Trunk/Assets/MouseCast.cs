ebe86a685667f26ea816b48f2cf5d254786fc0d6
hafdise <hafdis13@ru.is>
2016-03-04 16:54:39 +0000

    oculusery

--- Trunk/Assets/MouseCast.cs
+++ Trunk/Assets/MouseCast.cs
@@ -1,18 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Boardgame;
 
 public class MouseCast : MonoBehaviour {
 
+	PhysicalCell cell; 
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		var v3 = Input.mousePosition;
-		v3.z = 0.5f;
-		v3 = Camera.main.ScreenToWorldPoint(v3);
-		transform.position = v3;
+	void LateUpdate () {
+		transform.position = Camera.main.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0.3f));
+		if(cell != null && Input.GetKeyUp(KeyCode.Space)) {
+			cell.OnMouseUp();
+		}
+	}
+
+	void FixedUpdate() {
+		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (0.5f, 0.5f, 0.3f));
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit, 3f)) {
+			if(cell != null) cell.OnMouseExit();
+			var newCell = hit.collider.gameObject.GetComponent<PhysicalCell>();
+			if(newCell != null) {
+				newCell.OnMouseEnter();
+				cell = newCell;
+			}
+		}
 	}
 }
