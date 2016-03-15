4929354760203ff85bcb810f2c78f4920f86c6c1
Hafdis Erla <hafdiseh@gmail.com>
2016-03-15 17:03:19 +0000

    IK stuff

---
+++ Trunk/Assets/IK/Editor/IKSegmentEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace IK {
+    [CustomEditor(typeof(IKSegment))]
+    public class IKSegmentEditor : Editor {
+        IKSegment segment;
+
+        void OnEnable() {
+            segment = target as IKSegment;
+            segment.Init();
+        }
+
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+        }
+
+        public void OnSceneGUI() {
+            UnityEditor.Tools.current = Tool.None;
+            EditorGUI.BeginChangeCheck();
+            Quaternion rot = Handles.RotationHandle(segment.transform.rotation, segment.JointBase+segment.OriginalPosition);
+            if (EditorGUI.EndChangeCheck()) {
+                Undo.RecordObject(target, "Rotated Segment");
+                segment.Rotate(rot.eulerAngles);
+            }
+        }
+    }
+}
---
+++ Trunk/Assets/IK/Scripts/IKSegment.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace IK {
+    public class IKSegment : MonoBehaviour {
+        public Vector3 JointBase;
+        public Vector3 JointEnd;
+        public Vector3 RotationConstraintsUpper;
+        public Vector3 RotationConstraintsLower;
+        private Vector3 originalRotation;
+        public Vector3 OriginalPosition;
+
+        // Use this for initialization
+        void Awake() {
+            Init();
+        }
+
+        public void Init() {
+            originalRotation = transform.localEulerAngles;
+            OriginalPosition = transform.position;
+        }
+
+        void LateUpdate() {            
+
+        }
+
+        //honestly just testing purposes,
+        //the resolver should take care to observe and preserve
+        //constraints
+        public void Rotate(Vector3 euler) {
+            Vector3 newRotation;
+            transform.RotateAround(JointBase + OriginalPosition, Vector3.right, euler.x);
+            transform.RotateAround(JointBase + OriginalPosition, Vector3.up, euler.y);
+            transform.RotateAround(JointBase + OriginalPosition, Vector3.forward, euler.z);
+            newRotation = transform.localEulerAngles;
+            //TODO: clamp to constraints
+        }
+    }
+}
