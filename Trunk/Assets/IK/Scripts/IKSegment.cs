ca3c1733c3ee4f50a2b3e875bdd3268262aec005
Hafdis Erla <hafdiseh@gmail.com>
2016-03-16 14:25:25 +0000

    nothing specific, fiddling back and forth, need to discuss ideas

--- Trunk/Assets/IK/Scripts/IKSegment.cs
+++ Trunk/Assets/IK/Scripts/IKSegment.cs
@@ -2,37 +2,11 @@
 using System.Collections;
 namespace IK {
     public class IKSegment : MonoBehaviour {
-        public Vector3 JointBase;
-        public Vector3 JointEnd;
+        public string ID;
         public Vector3 RotationConstraintsUpper;
         public Vector3 RotationConstraintsLower;
-        private Vector3 originalRotation;
-        public Vector3 OriginalPosition;
 
-        // Use this for initialization
         void Awake() {
-            Init();
-        }
-
-        public void Init() {
-            originalRotation = transform.localEulerAngles;
-            OriginalPosition = transform.position;
-        }
-
-        void LateUpdate() {            
-
-        }
-
-        //honestly just testing purposes,
-        //the resolver should take care to observe and preserve
-        //constraints
-        public void Rotate(Vector3 euler) {
-            Vector3 newRotation;
-            transform.RotateAround(JointBase + OriginalPosition, Vector3.right, euler.x);
-            transform.RotateAround(JointBase + OriginalPosition, Vector3.up, euler.y);
-            transform.RotateAround(JointBase + OriginalPosition, Vector3.forward, euler.z);
-            newRotation = transform.localEulerAngles;
-            //TODO: clamp to constraints
         }
     }
 }
