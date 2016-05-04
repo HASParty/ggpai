cb439daba8aa44713ae31da09432d1c5e8f1a9c3
Garviell <faustusxi@gmail.com>
2016-05-04 18:47:14 +0000

    refactoring

--- ggp/src/main/java/gamer/MCTS/MCTSControlValues.java
+++ ggp/src/main/java/gamer/MCTS/MCTSControlValues.java
@@ -3,20 +3,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MCTSControlValues{
-    public long rave;
-    public long grave;
-    public long limit;
-    public double randErr;
-    public double treeDiscount;
-    public double chargeDiscount;
-    public double epsilon;
-    public long chargeDepth;
-    public long horizon;
-    public long explorationFactor;
-    public ArrayList<Double> defensiveness;
-    public ArrayList<Double> aggression;
-    public ArrayList<Double> chargeDefaults;
-    public boolean changed;
+    public long rave;                       // Rave K value to set linear rave weight decrease
+    public long grave;                      // Threshold at which to stop using grave
+    public long limit;                      // The simulation limit
+    public double randErr;                  // The odds of randomly picking a move instead of picking the best
+    public double treeDiscount;             // The goal discount inside of the tree
+    public double chargeDiscount;           // The goal discount in a depth charge
+    public double epsilon;                  // The odds of using MAST rather than picking a random move
+    public long chargeDepth;                // At which depth should a charge be stopped
+    public long horizon;                    // How deep should our tree be allowed to be
+    public long explorationFactor;          // UCT C value to control MCTS exploration
+    public ArrayList<Double> defensiveness; // Used to modulate goal weight of keeping pieces alive
+    public ArrayList<Double> aggression;    // Used to modulate goal weight of killing pieces
+    public ArrayList<Double> chargeDefaults;// The default values used if a charge is stopped
+    public boolean changed;                 // A control value used to see if a value has changed
 
     public MCTSControlValues(){
         explorationFactor = 40;
@@ -26,13 +26,13 @@ public class MCTSControlValues{
         epsilon = 0.9f;
         treeDiscount = 0.999f;
         chargeDiscount = 0.995f;
-        limit = 3000;
+        limit = 0;
         defensiveness = new ArrayList<>();
-        defensiveness.add(0.00);
-        defensiveness.add(0.00);
+        defensiveness.add(0.50);
+        defensiveness.add(0.50);
         aggression = new ArrayList<>();
-        aggression.add(0.00);
-        aggression.add(0.00);
+        aggression.add(0.50);
+        aggression.add(0.50);
         chargeDefaults = new ArrayList<>();
         chargeDefaults.add(40.00);
         chargeDefaults.add(40.00);
@@ -77,6 +77,7 @@ public class MCTSControlValues{
     public void setDefensiveness(List<Double> defensiveness){
         this.defensiveness = new ArrayList<>(defensiveness);
     }
+
     public void setChargeDefaults(List<Double> chargeDefaults){
         this.chargeDefaults = new ArrayList<>(chargeDefaults);
     }
@@ -97,7 +98,6 @@ public class MCTSControlValues{
     public void setRandErr(double randErr){
         this.randErr = randErr;
     }
-
 
     public void setAll(ArrayList<Double> values){
         setEpsilon(values.get(0));
